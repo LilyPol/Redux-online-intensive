0b10103b49b0f032737f79a0b73487aa8cc3816b
LilyPol <pollypin33@gmail.com>
2019-02-21 22:49:27 +0200

    lesson-10

--- jest/scripts/setupFiles.js
+++ jest/scripts/setupFiles.js
@@ -7,6 +7,13 @@ const errorMessage = 'TEST_ERROR_MESSAGE.';
 const token = 'TEST_TOKEN';
 const error = new Error(errorMessage);
 
+const users = {
+    id:        'TEST_ID',
+    avatar:    'TEST_AVATAR',
+    firstName: 'FIRST_NAME',
+    lastName:  'LAST_NAME',    
+};
+
 const userProfile = {
     id:        'TEST_ID',
     avatar:    'TEST_AVATAR',
@@ -48,6 +55,7 @@ const fetchResponseFail400 = {
 const url = 'https://www.url.com';
 
 global.__ = {
+    users,
     userProfile,
     errorMessage,
     token,
